97a79e883875a049a40e2903db3ae5e2ca5e1dc5
Mindy <mindynix@Sarahs-MacBook-Air.local>
2018-01-02 21:11:25 -0800

    updated Chatterbot

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,10 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = "LqZehyBrSOwzkGx7Cpwx4mhsK"
+consumer_secret = "k5taBhrfGgYNQ8ecW3JTbXibCb32f5kC375C8NF3CqAr7javKB"
+access_token = "133594066-M9KUh9H6JoDyva5bkTTRt8ojaZ9dCafHc9lazW1k"
+access_token_secret = "PMtstoGy0SR0yw4YqlQrge8ZRkpfy3aBeznE6WVsN8FC5"
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -15,5 +15,15 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
-# Create a function that tweets
-# CODE GOES HERE
+def TweetOut(tweet_number):
+	api.update_status("Happy New Year. This is a blessing to all of my friends. %s" %blessing)
+
+blessings=("May your hearts be kept warm.","May your hearts remain open.", "May your feet be wherever you want them to be")
+counter=0
+
+while(counter<len(blessings)):
+	TweetOut(blessings[counter])
+
+	time.sleep(60)
+
+	counter = counter + 1
